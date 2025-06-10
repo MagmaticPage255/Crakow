bc155f07bce912988eccf896a1d3e2c83790db2e
MagmaticPage255 <cammar83@kingshigh.school.nz>
2025-06-10 12:55:03 +1200

    update options.gd

--- Scenes/options.gd
+++ Scenes/options.gd
@@ -8,8 +8,11 @@ func _on_volume_value_changed(value: float) -> void:
 
 
 func _on_resolution_item_selected(index: int) -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	match index:
 		0:
 			DisplayServer.window_set_size(Vector2i(1920,1080))
 		1:
-			DisplayServer.window_set_size(Vector2i(1152,648))
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
