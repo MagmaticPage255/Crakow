284a26624ddecebdcbbfeff1adf4c5f7571795cf
edclo74 <edclo74@kingshigh.school.nz>
2025-07-18 12:10:32 +1200

    FixedTest World Pre Merge

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -15,3 +15,8 @@ func _on_button_3_pressed() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/TestFPS.tscn")
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/TestWorld.tscn")
+	pass # Replace with function body.
