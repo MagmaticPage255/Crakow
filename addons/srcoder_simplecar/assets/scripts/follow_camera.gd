8b8afff3a06181f5f48470a80cab9352a10091ad
MagmaticPage255 <cammar83@kingshigh.school.nz>
2025-08-21 12:24:45 +1200

    CAMERA

--- addons/srcoder_simplecar/assets/scripts/follow_camera.gd
+++ addons/srcoder_simplecar/assets/scripts/follow_camera.gd
@@ -18,8 +18,8 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	global_position = follow_target.global_position
-	var target_horizontal_direction = follow_target.global_basis.z.slide(Vector3.UP).normalized()
-	var desired_basis = Basis.looking_at(-target_horizontal_direction)
-	global_basis = global_basis.slerp(desired_basis,rotation_damping*delta)
+#func _physics_process(delta: float) -> void:
+#	global_position = follow_target.global_position
+#	var target_horizontal_direction = follow_target.global_basis.z.slide(Vector3.UP).normalized()
+#	var desired_basis = Basis.looking_at(-target_horizontal_direction)
+#	global_basis = global_basis.slerp(desired_basis,rotation_damping*delta)
