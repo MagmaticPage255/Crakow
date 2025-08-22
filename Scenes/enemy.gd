ba1d18556a13b471d5b029a6f2857c15862b254a
edclo74 <edclo74@kingshigh.school.nz>
2025-08-22 12:34:27 +1200

    epik stuff
    gegeefdf

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -4,6 +4,25 @@ extends CharacterBody3D
 @onready var animation_player: AnimationPlayer = $AuxScene/AnimationPlayer
 @onready var timer: Timer = $Timer
 var dead = false
+var gravity = 9.8
+var speed = 3.5
+@onready var nav = $NavigationAgent3D
+
+func _process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	else:
+		velocity.y -= 2
+	var next_location = nav.get_next_path_position()
+	var current_location = global_transform.origin
+	var new_velocity = (next_location - current_location).normalized() * speed
+	
+	velocity = velocity.move_toward(new_velocity,0.25)
+	move_and_slide()
+	
+func target_position(target):
+	nav.target_position = target
+
 func _ready():
 	animation_player.play("SadIdle")
 	timer.start()
@@ -21,6 +40,7 @@ func _on_timer_timeout() -> void:
 	timer.start()
 
 func hit():
+	if dead:return
 	timer.stop()
 	dead = true
 	animation_player.play("StandingReactDeathBackward")
--- player.gd
+++ player.gd
@@ -1,1 +1,6 @@
 extends CharacterBody3D
+
+@onready var target=$Player
+
+func _process(delta):
+	get_tree().call_group("Enemy", "target_position", target.global_transform.origin)
